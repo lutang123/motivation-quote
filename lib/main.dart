1af685aa2678611ffaacd4d06971ed6b645cbcc0
Lu Tang <lutang908@gmail.com>
2021-01-16 20:17:37 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import './quote_data.dart';
+import './favorite_quotes.dart';
+
+//https://medium.com/flutter-community/create-a-motivation-app-using-flutter-391de123a382
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        //??
+        home: DefaultTabController(
+          length: 2,
+          child: HomePage(),
+        ),
+      ),
+    );
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        //
+        titleSpacing: size.width / 5,
+        backgroundColor: Colors.black,
+        title: Text(
+          'Daily motivation',
+          style: TextStyle(fontFamily: 'quoteScript', fontSize: 22.0),
+        ),
+        bottom: TabBar(
+          tabs: <Widget>[
+            Tooltip(
+              message: 'Daily Quotes',
+              child: Tab(
+                icon: Icon(
+                  Icons.today,
+                ),
+              ),
+            ),
+            Tab(
+              icon: Icon(Icons.favorite),
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        children: <Widget>[
+          Stack(children: <Widget>[
+            Center(
+              child: Image.asset(
+                'images/background.jpg',
+                width: size.width,
+                height: size.height,
+                fit: BoxFit.fill,
+              ),
+            ),
+            QuoteData(),
+          ]),
+          FavoriteQuotes(),
+        ],
+      ),
+    );
+  }
+}
